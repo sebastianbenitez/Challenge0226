af1914fce1092f3dac04ac7a4f705b8b86e05a1d
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-02-28 04:00:17 -0300

    Actualizado punto de los killing sprees
    Descubri que entre los stats del participante estaba la propiedad win :/

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const getTotalKills = (kills, participant)=> kills + participant.stats.kills;
 const getTotalDeaths = (deaths, participant)=> deaths + participant.stats.deaths;
 
-// 1 Cantidad de Kills y Deaths de la partida
+// Cantidad de Kills y Deaths de la partida
 let gameTotalKills = LoLgame.participants.reduce(getTotalKills, 0);
 let gameTotalDeaths = LoLgame.participants.reduce(getTotalDeaths, 0);
 
@@ -12,40 +12,34 @@ console.log('Total deaths: ' + gameTotalDeaths);
 console.log('');
 
 
-LoLgame.teams.forEach((team, i)=> {
+LoLgame.teams.forEach((team)=> {
 
 	let teamMates = LoLgame.participants.filter((participant)=> participant.teamId === team.teamId);
-	
-	// solo para que salga bonito el indice
-	i++;
 
-	// 2 Cantidad de Kills y Deaths por cada equipo
+	// Cantidad de Kills y Deaths por cada equipo
 	let teamTotalKills = teamMates.reduce(getTotalKills, 0);
 	let teamTotalDeaths = teamMates.reduce(getTotalDeaths, 0);
 	
-	console.log(`Kills Team ${i}: ${teamTotalKills}`);
-	console.log(`Deaths Team ${i}: ${teamTotalDeaths}`);
+	console.log(`Kills Team ${team.teamId}: ${teamTotalKills}`);
+	console.log(`Deaths Team ${team.teamId}: ${teamTotalDeaths}`);
 	console.log('');
 
 
-	// 3 Del equipo que haya ganado, obtener un array con los participantes que hayan tenido 2 o más KillingSprees
-	if (team.win === 'Win') {		
-		let spreeKillers = teamMates.filter((participant)=> participant.stats.killingSprees > 1);
-		
-		console.log('Spree Killers Team Winner: ');
-		console.log(spreeKillers);
-		console.log('');
-	}
-
-
-	// 4 El SummonerName del player que haya tenido más kills en cada equipo
+	// El SummonerName del player que haya tenido más kills en cada equipo
 	let topKiller = teamMates.reduce((topKiller, participant) => 
 		participant.stats.kills > topKiller.stats.kills ?participant :topKiller);
-
 	let summonerName = LoLgame.participantIdentities.find((pIdentity)=> 
 		pIdentity.participantId === topKiller.participantId).player.summonerName;
 
-	console.log(`Top kills Team ${i}: ${topKiller.stats.kills}`);
+	console.log(`Top kills Team ${team.teamId}: ${topKiller.stats.kills}`);
 	console.log(`Player: ${summonerName}`);
 	console.log('');
 });
+
+// Del equipo que haya ganado, obtener un array con los participantes que hayan tenido 2 o más KillingSprees	
+let spreeKillers = LoLgame.participants.filter((participant)=> 
+	participant.stats.win && participant.stats.killingSprees > 1);
+
+console.log('Spree Killers Team Winner: ');
+console.log(spreeKillers);
+console.log('');
